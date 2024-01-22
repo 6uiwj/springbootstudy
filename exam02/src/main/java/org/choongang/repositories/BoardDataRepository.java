3b58c7dae10e5aba0a1c6baa68a0534149b76493
6uiwj <oterges00@gmail.com>
2024-01-22 09:17:42 +0900

    240122 JSQL

--- exam02/src/main/java/org/choongang/repositories/BoardDataRepository.java
+++ exam02/src/main/java/org/choongang/repositories/BoardDataRepository.java
@@ -1,11 +1,24 @@
 package org.choongang.repositories;
 
 import org.choongang.entities.BoardData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
     //BoardData엔티티에서 subject 필드를 가지고 검색을 할건데 'keyword'를 포함한 내용을 조회할 것임
     List<BoardData> findBySubjectContaining(String keyword);
+
+    //반환값이 페이지 (페이징에 관련된 편의기능 추가)
+    Page<BoardData> findBySubjectContaining(String keyword, Pageable pageable);
+    List<BoardData> findBySubjectContainingOrderBySeqDesc(String keyword);
+
+    //쿼리가 복잡할 때 @Query 어노테이션 이용(직접 JPA형태의 쿼리 입력)
+    //엔티티명 기준이기 때문에 무조건 별칭을 써야 함
+    @Query("SELECT b FROM BoardData b WHERE b.subject LIKE '%:key%' ORDER BY b.seq DESC")
+    List<BoardData> getSubjects(@Param("key") String keyword);
 }
--- exam02/src/test/java/org/choongang/jpaex/Ex04Test.java
+++ exam02/src/test/java/org/choongang/jpaex/Ex04Test.java
@@ -6,13 +6,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.Order.desc;
+
 @SpringBootTest
 @TestPropertySource(properties = "spring.profiles.active=test")
+//@Transactional //테스트할 때마다 DB에 같은 데이터가 쌓이므로 오류 발생
+// 테스트 후 데이터를 rollback 해주기 위해 @Transactonal 사용
 public class Ex04Test {
     @Autowired
     private BoardDataRepository repository;
@@ -32,7 +41,21 @@ public class Ex04Test {
 
     @Test
     void test1() {
-        List<BoardData> items = repository.findBySubjectContaining("1");
-        items.forEach(System.out::println);
+        //List<BoardData> items = repository.findBySubjectContaining("1");
+        //items.forEach(System.out::println);
+        List<BoardData> items = repository.findBySubjectContainingOrderBySeqDesc("목");
+        items.forEach(System.out::println); //item배열의 각 요소들을 출력
     }
+
+        @Test
+        void test2() {
+        //정렬: 엔티티기준으로 입력
+            Pageable pageable = PageRequest.of(0,3, Sort.by(desc("createdAt"),asc("seq")));
+            Page<BoardData> data = repository.findBySubjectContaining("목",pageable);
+
+            //Page의 .getContent(): 조회된 데이터를 리스트형태로 가져옴
+            List<BoardData> items = data.getContent();
+            items.forEach(System.out::println);
+        }
+
 }
